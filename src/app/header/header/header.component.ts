461c834109a45ea5c9da02a6651304d3d7845c00
alishs <shabaisuorosh@gmail.com>
2024-10-16 14:53:18 +0330

    initial: bootstrap , components , header component:30%

---
+++ src/app/header/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css'],
+})
+export class HeaderComponent {
+ 
+}
